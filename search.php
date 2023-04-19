2add9a6d67a17a7e8041fc32b6a00967c6586e43
Germonero <131135914+Germonero@users.noreply.github.com>
2023-04-19 13:10:17 +0200

    HTML & PHP files
    Added the first HTML dummy files & the first PHP files

---
+++ search.php
@@ -0,0 +1,17 @@
+<ul id="live-search-results" class="clearfix">
+        <li class="search-result faq">
+                <a href="#">Lorem ipsum dolor sit amet</a>
+        </li>
+        <li class="search-result faq">
+                <a href="#">consetetur sadipscing elitr</a>
+        </li>
+        <li class="search-result faq">
+                <a href="#">ed diam nonumy eirmod tempor</a>
+        </li>
+        <li class="search-result faq">
+                <a href="#">aliquyam erat, sed diam voluptua</a>
+        </li>
+        <li class="search-result faq">
+                <a href="#"> At vero eos et accusam et justo duo dolores et ea rebum</a>
+        </li>
+</ul>
